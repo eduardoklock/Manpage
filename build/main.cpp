39ef6e435b40b14b4da686e625727eb8e123a232
eduardoklock <eduardo.klock@gmail.com>
2014-07-13 01:01:56 -0300

    vai la trabalho

--- build/main.cpp
+++ build/main.cpp
@@ -1,30 +1,61 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
+#include <sstream>
 
-#include "dataStructures/CircularList.h"
+#include "doubly_linked_list.h"
 
 using std::cout;
 using std::endl;
 using std::string;
 using std::vector;
 
-using dataStructures::CircularList;
+using std::fstream;
+using std::stringstream;
 
-struct Index{
-	string word;
+struct Manual {
+
+	Manual(string name, string text){
+		this->name = name;
+		this->text = text;
+	}
+
+	string name;
+	string text;
 };
+
+
+vector<string> argumentVector(int argc, char** argv){
+	auto numberOfArguments = argc -1;
+	auto arguments = vector<string> {};
+
+	for(auto i = 0; i < numberOfArguments; i++){
+		arguments.push_back(argv[i + 1]);
+	}
+
+	return arguments;
+}
 
 int main(int argc, char** argv){
 
-	auto searchWordsSize = argc -1;
-	auto searchWords = vector<string> {};
+	auto fileNames = argumentVector(argc, argv);
 
-	for(auto i = 0; i < searchWordsSize; i++){
-		searchWords.push_back(argv[i +1]);
+	auto manuals = CircularList<Manual>();
+
+	for (string fileName : fileNames) {
+		fstream file;
+		file.open(fileName);
+		if(file.is_open()){
+			stringstream fileTextStream;
+			fileTextStream << file.rdbuf();
+			auto fileText = fileTextStream.str();
+
+			//
+
+			file.close();
+		}
 	}
 
-	for(auto word : searchWords){
-		cout << word << endl;
-	}
+	auto manpage = Manual(fileNames.at(0), fileText);
 }
